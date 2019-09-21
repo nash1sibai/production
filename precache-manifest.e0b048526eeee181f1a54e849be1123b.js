81605a97d0e0932b0e9e11df5d2750a78c84f9eb
Nashwan <nashwansebaee@yahoo.com>
2019-09-22 03:20:11 +0800

    Updates

---
+++ precache-manifest.e0b048526eeee181f1a54e849be1123b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a537b99d1b3f09b4aa4358601caa2a0d",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "19aa769887873682dd8f",
+    "url": "/production/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e0df5a1cfa584ed654bb",
+    "url": "/production/static/js/2.41f90f6f.chunk.js"
+  },
+  {
+    "revision": "19aa769887873682dd8f",
+    "url": "/production/static/js/main.5e8b7d69.chunk.js"
+  },
+  {
+    "revision": "341d8e1aec2c2a0247f7",
+    "url": "/production/static/js/runtime-main.772d9d35.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/production/static/media/logo.25bf045c.svg"
+  }
+]);
